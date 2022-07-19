9eceaf5c6103d408c4405fe67892a08fc887242d
KeerthiK23939 <54125768+KeerthiK23939@users.noreply.github.com>
2022-07-19 17:25:43 -0500

    working main

--- src/main/java/com/team1816/season/auto/modes/LivingRoomMode.java
+++ src/main/java/com/team1816/season/auto/modes/LivingRoomMode.java
@@ -1,7 +1,6 @@
 package com.team1816.season.auto.modes;
 
 import com.team1816.lib.auto.AutoModeEndedException;
-import com.team1816.lib.auto.actions.SeriesAction;
 import com.team1816.lib.auto.actions.TrajectoryAction;
 import com.team1816.lib.auto.actions.WaitAction;
 import com.team1816.lib.auto.modes.AutoModeBase;
